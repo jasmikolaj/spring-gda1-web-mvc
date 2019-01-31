8fe7431c327f4817260f001360bf78c10b582a07
Mikolaj Jaskulski <m.jaskulski@travactory.com>
2019-01-31 07:11:21 +0100

    Basic servlet example

--- src/main/java/pl/isa/controller/GreetingController.java
+++ src/main/java/pl/isa/controller/GreetingController.java
@@ -1,8 +1,10 @@
 package pl.isa.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
@@ -12,6 +14,16 @@ public class GreetingController {
 		model.addAttribute("name", "Miki!");
 
 		return "hello.jsp";
+	}
+
+	@RequestMapping(value = "/greetings")
+	public ModelAndView sayGreeting() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("hello2.jsp");
+		modelAndView.getModel().put("name", "Miki");
+		modelAndView.setStatus(HttpStatus.OK);
+
+		return modelAndView;
 	}
 
 }
